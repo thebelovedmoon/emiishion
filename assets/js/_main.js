d67bd5f36426251a59fb0770d88b965f75c1fd1a
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2025-08-10 11:58:46 +0800

    some more changes to be made

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -3,7 +3,7 @@ let vtuber = new URLSearchParams(window.location.search).get("vtuber");
 function designateVTuber() {
   let ajx = new XMLHttpRequest();
   ajx.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) { document.getElementById("replaceScreen").innerHTML = this.responseText; }
+    if (this.readyState == 4 && this.status == 200) { document.body.innerHTML = this.responseText; }
   };
   ajx.open("GET", `/emiishion/assets/text/${isVTuber(vtuber)}.txt`, true);
   ajx.send();
@@ -11,20 +11,29 @@ function designateVTuber() {
 }
 
 function isVTuber(nm) {
-  let fvc = document.createElement("link");
+  let fvc = document.createElement("link"),
+    scrpt = document.createElement("script"),
+    importCSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
   fvc.rel = "icon";
   fvc.type = "image/x-icon";
+  scrpt.type = "text/javascript";
   switch (nm) {
     case "EmiiShion":
       document.title = "Emii Shion (絵美紫苑)👑🐣";
       fvc.href = "/emiishion/assets/images/icon/pfp_Emii.png";
       document.head.appendChild(fvc);
+      scrpt.src = `/emiishion/assets/js/vtuber/${nm}.js`;
+      document.head.appendChild(scrpt);
+      importCSS(`@import url("/emiishion/assets/css/vtuber/${nm}.css");`);
       return nm;
     case "LeidramCh":
       document.title = "Arthur Regina Leidram (アーサー王)👑🐣";
       fvc.href = "/emiishion/assets/images/icon/pfp_Leidram.png";
       document.head.appendChild(fvc);
+      scrpt.src = `/emiishion/assets/js/vtuber/${nm}.js`;
+      document.head.appendChild(scrpt);
+      importCSS(`@import url("/emiishion/assets/css/vtuber/${nm}.css");`);
       return nm;
-    default: return null;
+    default: return event.preventDefault();
   }
 }
---
+++ assets/js/vtuber/LeidramCh.js
@@ -0,0 +1,1 @@
+console.log("Arthur Regina Leidram");
